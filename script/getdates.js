0a40286372523ce2bc5b6a4eba0128d7985eb2ee
mefix007 <odu24002@byui.edu>
2024-04-30 20:26:04 +0100

    week02 media query sample

--- script/getdates.js
+++ script/getdates.js
@@ -1,2 +1,6 @@
-const d = new Date();
-document.getElementById("modified").innerHTML = year;
+// const d = new Date();
+// document.getElementById("modified").innerHTML = year;
+let text = document.lastModified;
+document.getElementById("demo").innerHTML = text;
+const date = new Date(document.lastModified);
+document.getElementById("demo").innerHTML = date;
