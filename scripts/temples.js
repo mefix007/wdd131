f6b098242f9046b704c6275c2fcbc917e8d2d4eb
mefix007 <odu24002@byui.edu>
2024-05-17 01:05:19 +0100

    temples.html updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,9 @@
 // Get DOM Elements
 const currentYear = document.querySelector('#year');
-const lastModification = document.querySelector('#demo');
-// const hamButton = document.querySelector('#menu');
-// const navigation = document.querySelector('nav');
-// const ul = document.querySelector("ul");
+const lastModification = document.querySelector('#lastModification');
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
 // Get Dates
 const today = new Date()
 const lastModificationDate = new Date(document.lastModified)
@@ -13,18 +13,7 @@ currentYear.innerHTML = `${today.getFullYear()}`
 lastModification.innerHTML = `Last Modification: ${lastModificationDate}`
 
 // Eevents Listener
-// hamButton.addEventListener('click', () => {
-//     navigation.classList.toggle('open');
-//     hamButton.classList.toggle('open');
-
-// hamButton.addEventListener("click", () => {
-//     ul.classList.toggle("open-ul");
-//     hamButton.classList.toggle("open");
-// })
-const mainnav = document.querySelector('.navigation');
-const hambutton = document.querySelector('#menu');
-
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
-});
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+})
